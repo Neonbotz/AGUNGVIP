37c4cc892daef7ce0cd9b267cd83a25e24df434d
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-05 09:57:01 +0700

    Update owner-bc-all.js

--- plugins/owner-bc-all.js
+++ plugins/owner-bc-all.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  let chats = conn.chats.map().filter(v => v.jid.endsWith('.net')).map(v => v.jid)
+  let chats = conn.chats().filter(v => v.jid.endsWith('.net')).map(v => v.jid)
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_\nestimasi selesai ${chats.length * 1.5} detik`, m)
