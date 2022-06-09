ab02cdc157b46fe8939987557c3e18e2e213ca0f
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-09 19:04:14 +0700

    Update audio-toptt.js

--- plugins/audio-toptt.js
+++ plugins/audio-toptt.js
@@ -1,16 +1,18 @@
-const { toPTT } = require('../lib/converter')
+import { toPTT } from '../lib/converter.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan perintah *${usedPrefix + command}*`
-  let media = await q.download()
-  let audio = await toPTT(media, 'mp4')
-  conn.sendFile(m.chat, audio, '', '', m, 1, { mimetype: 'audio/mp4' })
+    let q = m.quoted ? m.quoted : m
+    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    if (!/video|audio/.test(mime)) throw `reply video/audio you want to convert to voice note/vn with caption *${usedPrefix + command}*`
+    let media = await q.download?.()
+    if (!media) throw 'Can\'t download media'
+    let audio = await toPTT(media, 'mp4')
+    if (!audio.data) throw 'Can\'t convert media to audio'
+    conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, true, { mimetype: 'audio/mp4' })
 }
-handler.help = ['tovn']
+handler.help = ['tovn (reply)']
 handler.tags = ['audio']
 
 handler.command = /^to(vn|(ptt)?)$/i
 
-module.exports = handler
+export default handler
