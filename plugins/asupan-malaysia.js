79e47a397e95eda58cf1074d1f7ed32dab15fb98
Neon mD <106319373+Neonbotz@users.noreply.github.com>
2022-06-27 14:05:18 +0700

    Update asupan-malaysia.js

--- plugins/asupan-malaysia.js
+++ plugins/asupan-malaysia.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zacros.my.id/asupan/malaysia'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.malaysia', m)
+    conn.sendFile(m.chat, api-hyzer, null, 'Nih', m)
 }
 handler.help = ['malaysia']
 handler.tags = ['asupan']
